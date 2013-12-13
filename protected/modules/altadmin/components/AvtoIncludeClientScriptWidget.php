6c9a26706fe116c6c895c3e5406eefdae76eb5ae
Egor Rihnov <egor.developer@gmail.com>
2013-12-13 14:21:29 +0200

    Автоматическое подключение js и css скиптов

---
+++ protected/modules/altadmin/components/AvtoIncludeClientScriptWidget.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of UploadifyWidget
+ *
+ * @author egorik
+ */
+class ImagesListWidget extends CWidget {
+    public $pid;
+    public $folder;
+    public $model;
+    public $modelId;
+    public function run() {
+        $imgModel = new $this->model;
+        $imgList =  $imgModel->findAll(array('condition'=>'`pid`="'.$this->pid.'"', 'order'=>'position DESC'));
+        $this->render('imagesListWidget', array('imgList'=>$imgList, 'folder' => $this->folder, 'model'=>$this->model, 'modelId'=>$this->modelId));
+    }
+}
+
+?>
