b0780bcded68184de61769dbade78f6eaa5f7e80
Egor Rihnov <egor.developer@gmail.com>
2013-12-26 13:04:41 +0200

    Правка блока цитат

--- protected/components/views/quoteWidget.php
+++ protected/components/views/quoteWidget.php
@@ -1,4 +1,4 @@
 <blockquote>
 <p class="l-quote"><?php echo $model->text; ?></p>
-<p class="l-quote-author"><?php echo $model->author; ?></p>
+<small class="l-quote-author"><?php echo $model->author; ?></small>
 </blockquote>
