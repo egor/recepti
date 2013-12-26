49015996f38dc028a46c5aefbcab9e0d218120d8
Egor Rihnov <egor.developer@gmail.com>
2013-12-26 13:04:28 +0200

    Правка карты сайта

--- protected/controllers/SiteMapXMLController.php
+++ protected/controllers/SiteMapXMLController.php
@@ -5,7 +5,7 @@ class SiteMapXMLController extends Controller
     public $inSiteMap = array (
         'category' => true,
         'dishes' => true,
-        'news' => false,
+        'news' => true,
         'pages' => true,
         'other' => true
     );
@@ -15,21 +15,34 @@ class SiteMapXMLController extends Controller
         if ($this->inSiteMap['category']) {
             $xml .= $this->catalogMap();
         }
+        if ($this->inSiteMap['news']) {
+            $xml .= $this->newsMap();
+        }
         $this->renderPartial('index', array('xml' => $xml));
 	}
     
     protected function catalogMap()
     {
         $text = '<url><loc>http://recepti.dp.ua/recipes</loc></url>';
-        $category = Category::model()->findAll(array('condition' => 'visibility = 1', 'select' => 'url', 'order' => 'menu_name'));
+        $category = Category::model()->findAll(array('condition' => 'visibility = 1', 'select' => 'url, category_id', 'order' => 'menu_name'));
         foreach ($category as $categoryValue) {
             $text .= '<url><loc>http://recepti.dp.ua/recipes/'.$categoryValue->url.'</loc></url>';
             if ($this->inSiteMap['dishes']) {
-                $recipes = Dishes::model()->findAll(array('condition' => 'visibility = 1', 'select' => 'url', 'order' => 'menu_name'));
+                $recipes = Dishes::model()->findAll(array('condition' => 'visibility = 1 AND category_id = "' . $categoryValue->category_id . '"', 'select' => 'url', 'order' => 'menu_name'));
                 foreach ($recipes as $recipesValue) {
-                    $text .= '<url><loc>http://recepti.dp.ua/recipes/'.$categoryValue->url.'/'.$categoryValue->url.'</loc></url>';
+                    $text .= '<url><loc>http://recepti.dp.ua/recipes/'.$categoryValue->url.'/'.$recipesValue->url.'</loc></url>';
                 }
             }
+        }
+        return $text;
+    }
+    
+    protected function newsMap()
+    {
+        $text = '<url><loc>http://recepti.dp.ua/news</loc></url>';
+        $news = News::model()->findAll(array('condition' => 'visibility = 1', 'select' => 'url', 'order' => 'date DESC, news_id DESC'));
+        foreach ($news as $newsValue) {
+            $text .= '<url><loc>http://recepti.dp.ua/news/'.$newsValue->url.'</loc></url>';            
         }
         return $text;
     }
